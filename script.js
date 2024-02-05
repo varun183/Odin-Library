10adda10b40e45c3e505ba9be0c67c00ff8689c2
varun183 <125751714+varun183@users.noreply.github.com>
2024-02-06 01:31:44 +0530

    add display logic of the book

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const addBookDiv = document.querySelector('.addBook')
+const addBookBtn = document.createElement('button');
+addBookBtn.textContent = 'Add Book';
+addBookBtn.setAttribute('style','color:white; background:black; ');
+addBookBtn.classList.add('addButton');
+addBookDiv.appendChild(addBookBtn);
+
+const BookInfo =  document.querySelector( '.displayBooks .card .bookInfo');
+
+
+
+
 function Book(title,author,pages,read){
 
     this.title = title;
@@ -11,5 +23,35 @@ function Book(title,author,pages,read){
 
 }
 
-const Book1 = new Book('The Hobbit' ,'J.R.R. Tolkien','295',true);
-console.log(Book1.info());
+const myLibrary = [
+   
+];
+
+
+
+function addBookToLibrary(){
+    let bookTitle = prompt("Enter Book Title:");
+    let authorName = prompt("Enter Author's name:");
+    let totalPages = prompt("Enter number of Pages:");
+    let readOrNot = prompt("Have you read the book?true or false");
+    let read = (readOrNot.toLowerCase() ==='true');
+
+    const newBook = new Book(bookTitle, authorName, totalPages, read);
+    myLibrary.push(newBook);
+
+
+}
+addBookBtn.addEventListener('click', function() {
+    addBookToLibrary();
+    let bookTitles = ""; // Initialize an empty string to store book titles
+    myLibrary.forEach(book => {
+        // Accumulate each book title into the bookTitles string
+        bookTitles += book.title + "\n";
+    });
+    // Set the accumulated book titles to the BookInfo element
+    BookInfo.textContent = bookTitles;
+});
+
+
+
+
